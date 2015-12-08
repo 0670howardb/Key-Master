3611dff30fe1f3434c899413f4180771fbd56fc7
Brandon Howard <brandon.m.howard@icloud.com>
2015-12-08 11:40:49 -0600

    Small fix to heart and soul

--- Sound.h
+++ Sound.h
@@ -6,23 +6,6 @@
 void DACInit(void);
 void playSound(int);
 extern uint16_t pitch;
-
-/*
-extern uint16_t c0Pitch;
-extern uint16_t cSharpPitch;
-extern uint16_t dPitch;
-extern uint16_t dSharpPitch;
-extern uint16_t ePitch;
-extern uint16_t fPitch;
-extern uint16_t fSharpPitch;
-extern uint16_t gPitch;
-extern uint16_t gSharpPitch;
-extern uint16_t aPitch;
-extern uint16_t aSharpPitch;
-extern uint16_t bPitch;
-extern uint16_t c1Pitch;
-*/
-
 extern int sinWave[];
 
 #endif
